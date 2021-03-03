ba0b7875befd7b9e31fe7dd06e963907b58a1518
Almetov Kamil <almetov.kamil@gmail.com>
2021-03-03 22:43:19 +0400

    Исправлена работа уведомлений для API 28+

--- app/src/main/java/com/example/joke/DelayedMessageService.java
+++ app/src/main/java/com/example/joke/DelayedMessageService.java
@@ -2,11 +2,15 @@ package com.example.joke;
 
 import android.app.IntentService;
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.TaskStackBuilder;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
 //import android.os.Handler;
 //import android.util.Log;
 //import android.widget.Toast;
@@ -16,6 +20,7 @@ public class DelayedMessageService extends IntentService {
     public static final String EXTRA_MESSAGE = "message";
     //private Handler handler;
     public static final int NOTIFICATION_ID = 5453;
+    public static final String CHANNEL_ID = "123";
 
     public DelayedMessageService() {
         super("DelayedMessageService");
@@ -40,6 +45,22 @@ public class DelayedMessageService extends IntentService {
         showText(text);
     }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
+
     private void showText(final String text) {
         //Log.v("DelayedMessageService", "The message is: " + text);
         /*handler.post(new Runnable() {
@@ -48,6 +69,7 @@ public class DelayedMessageService extends IntentService {
                 Toast.makeText(getApplicationContext(), text, Toast.LENGTH_LONG).show();
             }
         });*/
+        createNotificationChannel();
         Intent intent = new Intent(this, MainActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
         stackBuilder.addParentStack(MainActivity.class);
@@ -55,12 +77,12 @@ public class DelayedMessageService extends IntentService {
         PendingIntent pendingIntent =
                 stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT
                 );
-        Notification notification = new Notification.Builder(this)
+        Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setContentTitle(getString(R.string.app_name))
                 .setAutoCancel(true)
-                .setPriority(Notification.PRIORITY_MAX)
-                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+                .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
                 .setContentIntent(pendingIntent)
                 .setContentText(text)
                 .build();
